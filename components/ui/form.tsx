fa8e7e854262d78d9117948dd8e8379facd74588
唐诗轶 <1787129710@qq.com>
2026-03-31 21:17:03 +0800

    fix: 修复 form.tsx 校验错误不显示的问题

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -7,7 +7,6 @@ import {
   Controller,
   FormProvider,
   useFormContext,
-  useFormState,
   type ControllerProps,
   type FieldPath,
   type FieldValues,
@@ -45,8 +44,7 @@ const FormField = <
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext)
   const itemContext = React.useContext(FormItemContext)
-  const { getFieldState } = useFormContext()
-  const formState = useFormState({ name: fieldContext.name })
+  const { getFieldState, formState } = useFormContext()
   const fieldState = getFieldState(fieldContext.name, formState)
 
   if (!fieldContext) {
